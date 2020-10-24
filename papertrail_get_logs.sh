baec6d3c554555576b820885769af5818a7b25b5
Meet Patel <patelmeet59@gmail.com>
2020-10-24 21:08:38 +0530

    add script: Papertrail Logs archive downloader
    Signed-off-by: Meet Patel <patelmeet59@gmail.com>

---
+++ papertrail_get_logs.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+PAPERTRAIL_TOKEN="ENTER YOUR PAPERTRAIL TOKEN HERE"
+
+mkdir -p ./logs
+DATE=23
+MONTH=10
+YEAR=2020
+for HOUR in {0..23}; do
+  if [[ $HOUR -lt 10 ]]; then
+    curl --no-include -o ./logs/$YEAR-$MONTH-$DATE-0$HOUR.tsv.gz -L -H "X-Papertrail-Token: $PAPERTRAIL_TOKEN" \
+        https://papertrailapp.com/api/v1/archives/$YEAR-$MONTH-$DATE-0$HOUR/download
+  else
+    curl --no-include -o ./logs/$YEAR-$MONTH-$DATE-$HOUR.tsv.gz -L -H "X-Papertrail-Token: $PAPERTRAIL_TOKEN" \
+        https://papertrailapp.com/api/v1/archives/$YEAR-$MONTH-$DATE-$HOUR/download
+  fi
+done
