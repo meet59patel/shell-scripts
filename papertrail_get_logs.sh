28bc3c5b628f629de3ddbc651b0ecad7fa63e626
Meet Patel <patelmeet59@gmail.com>
2020-10-25 17:45:09 +0530

    fix: papertrail_logs folder
    Signed-off-by: Meet Patel <patelmeet59@gmail.com>

--- papertrail_get_logs.sh
+++ papertrail_get_logs.sh
@@ -10,10 +10,10 @@ mkdir -p ./$DATE-$MONTH-$YEAR
 
 for HOUR in {0..23}; do
   if [[ $HOUR -lt 10 ]]; then
-    curl --no-include -o ./logs/$YEAR-$MONTH-$DATE-0$HOUR.tsv.gz -L -H "X-Papertrail-Token: $PAPERTRAIL_TOKEN" \
+    curl --no-include -o ./$DATE-$MONTH-$YEAR/$YEAR-$MONTH-$DATE-0$HOUR.tsv.gz -L -H "X-Papertrail-Token: $PAPERTRAIL_TOKEN" \
         https://papertrailapp.com/api/v1/archives/$YEAR-$MONTH-$DATE-0$HOUR/download
   else
-    curl --no-include -o ./logs/$YEAR-$MONTH-$DATE-$HOUR.tsv.gz -L -H "X-Papertrail-Token: $PAPERTRAIL_TOKEN" \
+    curl --no-include -o ./$DATE-$MONTH-$YEAR/$YEAR-$MONTH-$DATE-$HOUR.tsv.gz -L -H "X-Papertrail-Token: $PAPERTRAIL_TOKEN" \
         https://papertrailapp.com/api/v1/archives/$YEAR-$MONTH-$DATE-$HOUR/download
   fi
 done
